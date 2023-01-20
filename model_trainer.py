9e0585e256b401ecb8396767decb2a247db567e7
Rehaan1 <rehaanmazid@gmail.com>
2023-01-20 18:35:19 +0530

    feat: preprocessing for type

--- model_trainer.py
+++ model_trainer.py
@@ -11,6 +11,31 @@ import matplotlib.pyplot as plt
 
 def reading_dataset():
     df = pd.read_csv('malicious_phish.csv')
-    print(df.head)
+    df = data_preprocessing(df)
+    print(df.head())
+
+def data_preprocessing(df: pd.DataFrame):
+
+    # Changing Values to Malicious and Safe
+    print("---------------------------------------")
+    print("Processing to Change Values to Malicious and Safe ......")
+    df = df.replace({'phishing': 'malicious'}, regex=True)
+    df = df.replace({'defacement': 'malicious'}, regex=True)
+    df = df.replace({'benign': 'safe'}, regex=True)
+    df = df.replace({'malware': 'malicious'}, regex=True)
+    
+    # Removing Null Values if any
+    print("---------------------------------------")
+    print("Delete Null Value..............")
+    df = df[pd.notnull(df['url'])]
+    
+    #Label Encode type
+    print("---------------------------------------")
+    print("Label Encoding Type..............")
+    df['category_id'] = df['type'].factorize()[0]
+    
+    
+    return df
+    
 
 reading_dataset()
